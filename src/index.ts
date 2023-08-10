cfc9f4a0e15ff1a269e7df38f16dc3de39476099
Emil Lehmann <emil-thomas.lehmann@telekom.de>
2023-08-10 10:32:22 +0200

    feat: Added func to set equal card height

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,33 @@ function init() {
   */
 }
 
+/*
+ * @param cards - Array of cards
+ * @returns maxHeight of all cards that are displayed on the page as Integer
+ * this iterates over array of cards and reads the height of each card to get the maximum height of all cards
+ */
+function getMaxHeight(cards) {
+  let maxHeight = 0;
+  cards.forEach((card) => {
+    const cardHeight = card.offsetHeight;
+    if (cardHeight > maxHeight) {
+      maxHeight = cardHeight;
+    }
+  });
+  return maxHeight;
+}
+
+/*
+ * @param cards - Array of cards
+ * @param height - height value which the card height should be set to
+ * this iterates over array of cards and sets the height of each card to the given input
+ */
+function setEqualHeight(cards, height) {
+  cards.forEach((card) => {
+    card.style.minHeight = `${height}px`;
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   init();
 
@@ -15,4 +42,9 @@ document.addEventListener("DOMContentLoaded", () => {
   if (main) {
     main.innerHTML = `Let's go`;
   }
+
+  const cards = document.querySelectorAll(".card");
+  const maxHeight = getMaxHeight(cards);
+
+  setEqualHeight(cards, maxHeight);
 });
