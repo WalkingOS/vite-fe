5fcaf6762cb7e5e21e02140cc7a31dcb0659f15b
Alexander Berz <Alexander.Berz@telekom.de>
2023-08-21 14:30:08 +0200

    foo

--- src/index.js
+++ src/index.js
@@ -3,20 +3,19 @@
 import "./style.scss";
 
 function init() {
-    /*   
+  /*   
       foo1();
       foo2(); 
     */
 }
 
-//Using JavaScript, identify all the card elements within the <ul> list:  
-
+//Using JavaScript, identify all the card elements within the <ul> list:
 
 //Determine the maximum height among all the cards:
 
 //schleife für ein array
 
-function determineMaxHeight(cards){
+function determineMaxHeight(cards) {
   let maxHeight = 0; //Variable um die endgültige maxHöhe zu speichern
   cards.forEach((card) => {
     const cardHeight = card.clientHeight;
@@ -31,57 +30,44 @@ function determineMaxHeight(cards){
 
 // Anpassen der Karten auf die maximale Höhe
 
-function applyMaxHeight(cards, maxHeight){
+function applyMaxHeight(cards, maxHeight) {
   cards.forEach((card) => {
-    card.style.height = maxHeight + 'px';
+    card.style.height = maxHeight + "px";
   });
 }
-  
 
 document.addEventListener("DOMContentLoaded", () => {
-    init();
+  init();
 
-    const main = document.querySelector("#main");
+  const main = document.querySelector("#main");
 
-    if (main) {
-        main.innerHTML = `Let's go`;
-    }
+  if (main) {
+    main.innerHTML = `Let's go`;
+  }
 
-    
-    const cards = document.querySelectorAll(".card");
-    applyMaxHeight(cards, determineMaxHeight(cards));
-    
-  
-    
+  const cards = document.querySelectorAll(".card");
+  applyMaxHeight(cards, determineMaxHeight(cards));
 
-// For each card, find the button element within the card's content.
+  // For each card, find the button element within the card's content.
 
-const button = card.querySelector(".card-button");
-
-// Attach a click event listener to each button.
-
-if (button) {
-    button.addEventListener("click", function() {
-
-// Inside the event listener, retrieve the headline text from the clicked card.
-// Überschriftstext der Karte abrufen
-        const headline = card.querySelector(".card-headline");
-        if (headline) {
-            const headlineText = headline.textContent;            
-
-// Use this headline text to display an alert to the user.
-// Alert mit Überschriftstext anzeigen
-            alert(headlineText);
-        }
-      
-
+function alertHeadline(cards){
+  cards.forEach((card) => {
+    const headline = card.querySelector(".card-headline");
+    const button = card.querySelector(".card-button");
+    if (headline) {
+      const message = headline.textContent;
+      button.addEventListener("click", () => {
+        window.alert(message));
+      }
     });
+  }
+
 }
-    });
+
+
+  // Attach a click event listener to each button.
+
+ 
 
 
 window.addEventListener("resize", adjustCardHeights);
-
-
-
- 
